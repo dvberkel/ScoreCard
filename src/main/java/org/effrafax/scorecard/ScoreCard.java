4eddb5f84cc4fc84c0f7032dd6d3e7a2eee708ea
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-22 14:52:17 +0100

    [REFACTOR] Verifiers abstracted
    Verifiers for rounds are abstraced. This is done to ensure a uniform
    treatment of the verification process.

--- src/main/java/org/effrafax/scorecard/ScoreCard.java
+++ src/main/java/org/effrafax/scorecard/ScoreCard.java
@@ -41,19 +41,98 @@ public class ScoreCard {
 	}
 
 	private void verify(Round round) {
-		if (! players.equals(round.players())) {
-			throw new WrongPlayerInRound();
+		for (RoundVerifier verifier : verifiers()) {
+			verifier.verify(round);
 		}
-		if (numberOfCardsThisRound() == round.bidTotal()) {
-			throw new BidsTotalsNumberOfCards();
-		}
-		if (numberOfCardsThisRound() != round.winnings()) {
-			throw new WinningsNotNumberOfCards();
-		}
+	}
+
+	private List<RoundVerifier> verifiers() {
+		List<RoundVerifier> verifiers = new ArrayList<RoundVerifier>();
+		verifiers.add(new PlayersVerifier(players));
+		verifiers.add(new BitTotalVerifier(numberOfCardsThisRound()));
+		verifiers.add(new WinningsVerifier(numberOfCardsThisRound()));
+		return verifiers;
 	}
 
 	private int numberOfCardsThisRound() {
 		return rounds.size() + 1;
+	}
+
+}
+
+interface RoundVerifier {
+	public void verify(Round round);
+}
+
+abstract class ConditionedRoundVerifier implements RoundVerifier {
+	@Override
+	public void verify(Round round) {
+		if (conditionOn(round)) {
+			throw exceptionForCondition();
+		}
+	}
+
+	public abstract boolean conditionOn(Round round);
+
+	public abstract RuntimeException exceptionForCondition();
+}
+
+class PlayersVerifier extends ConditionedRoundVerifier {
+
+	private final Set<String> players;
+
+	public PlayersVerifier(Set<String> players) {
+		this.players = players;
+	}
+
+	@Override
+	public boolean conditionOn(Round round) {
+		return ! players.equals(round.players());
+	}
+
+	@Override
+	public RuntimeException exceptionForCondition() {
+		return new WrongPlayerInRound();
+	}
+
+}
+
+class BitTotalVerifier extends ConditionedRoundVerifier {
+
+	private final int numberOfCardsThisRound;
+
+	public BitTotalVerifier(int numberOfCardsThisRound) {
+		this.numberOfCardsThisRound = numberOfCardsThisRound;
+	}
+
+	@Override
+	public boolean conditionOn(Round round) {
+		return numberOfCardsThisRound == round.bidTotal();
+	}
+
+	@Override
+	public RuntimeException exceptionForCondition() {
+		return new BidsTotalsNumberOfCards();
+	}
+
+}
+
+class WinningsVerifier extends ConditionedRoundVerifier {
+
+	private final int numberOfCardsThisRound;
+
+	public WinningsVerifier(int numberOfCardsThisRound) {
+		this.numberOfCardsThisRound = numberOfCardsThisRound;
+	}
+
+	@Override
+	public boolean conditionOn(Round round) {
+		return numberOfCardsThisRound != round.winnings();
+	}
+
+	@Override
+	public RuntimeException exceptionForCondition() {
+		return new WinningsNotNumberOfCards();
 	}
 
 }
